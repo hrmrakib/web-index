28a8646afbe7a87afb3ecdc187b78e5c1be74b06
hrmrakib <hrmrakibs@gmail.com>
2024-07-10 12:09:29 +0600

    implement slider dynamic

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,13 @@ document.getElementById("close").addEventListener("click", function () {
   let dropdown = document.getElementById("dropdown");
   dropdown.classList.add("hidden");
 });
+
+// slider scrolling
+function goBackSlide() {
+  const container = document.querySelector(".carousel");
+  container.scrollBy({ left: -container.clientWidth, behavior: "smooth" });
+}
+function goForwardSlide() {
+  const container = document.querySelector(".carousel");
+  container.scrollBy({ left: container.clientWidth, behavior: "smooth" });
+}
